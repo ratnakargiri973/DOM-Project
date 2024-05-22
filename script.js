bb180328c90b1696b68581f363cdd9cae3307851
Ratnakar <ratnakargiri973@gmail.com>
2024-05-22 12:44:30 +0530

    Dom Project 1 files added

---
+++ script.js
@@ -0,0 +1,9 @@
+let divElement=document.getElementById("myDiv");
+divElement.style.backgroundColor= "yellow";
+divElement.style.color= "blue";
+divElement.style.margin= "20px";
+divElement.style.padding= "10px";
+divElement.style.fontSize= "18px";
+divElement.style.fontWeight= "bold";
+divElement.style.height= "200px";
+divElement.style.width= "300px";
